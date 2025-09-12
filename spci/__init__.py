c4b8ef2a749423d97f42446d7abd22e388627911
Jonas Santos Siqueira <jonas14.siqueira@live.com>
2025-09-12 19:28:28 -0300

    Create __init__.py

---
+++ spci/__init__.py
@@ -0,0 +1,5 @@
+from .core import SPCI
+from .models import QRFQuantileRegressor, KNNQuantileRegressor
+from . import utils
+__all__ = ["SPCI", "QRFQuantileRegressor", "KNNQuantileRegressor", "utils"]
+__version__ = "1.0.0"
